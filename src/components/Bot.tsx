a1e83a792e477817a5d7ddda7aff622eb2b6c04b
salehwahab <bosloh.bx@gmail.com>
2025-06-30 19:22:58 +0900

    english to arabic

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -183,7 +183,8 @@ export type LeadsConfig = {
   successMessage?: string;
 };
 
-const defaultWelcomeMessage = "Hi there! Ready to find the perfect car? I'm here to help! \u202Bمرحبًا! هل أنت مستعد للعثور على السيارة المثالية؟ أنا هنا لمساعدتك\u202C";
+const defaultWelcomeMessage =
+  "Hi there! Ready to find the perfect car? I'm here to help! \u202Bمرحبًا! هل أنت مستعد للعثور على السيارة المثالية؟ أنا هنا لمساعدتك\u202C";
 
 /*const sourceDocuments = [
     {
